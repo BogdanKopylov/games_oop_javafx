409908b505b2ced3e961bafa1171e0ab6f29588c
BogdanKopylov <bogdankopylov@gmail.com>
2020-11-03 18:12:55 +0200

    2. Сокобан. Тестовое задание. [#384591]

--- puzzle/src/main/java/ru/job4j/puzzle/Win.java
+++ puzzle/src/main/java/ru/job4j/puzzle/Win.java
@@ -2,36 +2,39 @@ package ru.job4j.puzzle;
 
 public class Win {
     public static boolean check(int[][] board) {
-        return checkHorizontal(board) || checkVertical(board);
-    }
-    public static boolean checkHorizontal(int[][] board) {
-        for (int[] row : board) {
-            int count = 0;
-            for (int cell : row) {
-                if (cell == 0) {
-                    break;
-                } else if (cell == 1) {
-                    count++;
-                    if (count == board.length) {
-                        return true;
-                    }
-                }
+        for (int i = 0; i < board.length; i++) {
+            boolean isOne = board[i][i] == 1;
+            if (isOne && (checkHorizontal(board, i)) || (checkVertical(board, i))) {
+                return true;
             }
         }
         return false;
     }
 
-    public static boolean checkVertical(int[][] board) {
+    public static boolean checkHorizontal(int[][] board, int index) {
+        int count = 0;
         for (int i = 0; i < board.length; i++) {
-            int count = 0;
-            for (int[] ints : board) {
-                if (ints[i] == 0) {
+                if (board[index][i] == 0) {
                     break;
-                } else if (ints[i] == 1) {
+                } else if (board[index][i] == 1) {
                     count++;
                     if (count == board.length) {
                         return true;
                     }
+                }
+        }
+        return false;
+    }
+
+    public static boolean checkVertical(int[][] board, int index) {
+        int count = 0;
+        for (int[] ints : board) {
+            if (ints[index] == 0) {
+                break;
+            } else if (ints[index] == 1) {
+                count++;
+                if (count == board.length) {
+                    return true;
                 }
             }
         }
